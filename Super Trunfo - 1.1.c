567eb09d835181821b540f18a0f3b0662209ce95
iagonb-202502253036 <202502253036@alunos.newtonpaiva.edu.br>
2025-04-10 21:09:24 -0300

    Update Super Trunfo - 1.1.c

--- Super Trunfo - 1.1.c
+++ Super Trunfo - 1.1.c
@@ -2,27 +2,27 @@
 
 int main() {
 
-char letra1, letra2;
-char codigo1[50], codigo2[50];
-char cidade1[50], cidade2[50];
-int populacao1, populacao2;
-float km1, km2;
-float pib1, pib2;
+  char letra1, letra2;
+  char codigo1[50], codigo2[50];
+  char cidade1[50], cidade2[50];
+  int populacao1, populacao2;
+  float km1, km2;
+  float pib1, pib2;
 
-printf("Qual a letra da cidade?");
-printf("\n");
-scanf("%c", &letra1);
-printf("\n");
+  printf("Qual a letra da cidade?");
+  printf("\n");
+  scanf("%c", &letra1);
+  printf("\n");
 
-printf("Qual o codigo da cidade?");
-printf("\n");
-scanf("%s", &codigo1);
-printf("\n");
+  printf("Qual o codigo da cidade?");
+  printf("\n");
+  scanf("%s", &codigo1);
+  printf("\n");
 
-printf("Pressione Enter para sair...");
-getchar();
-getchar();
-
-return 0;
+  printf("Pressione Enter para sair...");
+  getchar();
+  getchar();
+  
+  return 0;
 
 }
